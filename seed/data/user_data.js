6677959b2df84472bfa496ceff262d234de7f211
Jenny Redfern <jchunyi.ho@googlemail.com>
2018-01-04 15:14:27 +0000

    Seed database and initial setup

---
+++ seed/data/user_data.js
@@ -0,0 +1,9 @@
+module.exports = [
+    {username: 'tickle122', name: 'Tom Tickle', avatar_url: 'https://dreager1.files.wordpress.com/2013/05/luffy_anime_post_timeskip_infobox.png'},
+    {username: 'grumpy19', name: 'Paul Grump', avatar_url: 'https://vignette.wikia.nocookie.net/vsbattles/images/d/d0/Roronoa_Zoro_Pre_Timeskip.png/revision/latest?cb=20160327033108'},
+    {username: 'happyamy2016', name: 'Amy Happy', avatar_url: 'https://orig00.deviantart.net/bfcd/f/2016/088/1/c/nami_one_piece__by_roxy_h___by_exsplo_d4ejmfq_by_yoos2210-d9wys7a.png'},
+    {username: 'cooljmessy', name: 'Peter Messy', avatar_url: 'http://www.behindthevoiceactors.com/_img/chars/tony-tony-chopper-toriko-x-one-piece-collaboration-special-32.5.jpg'},
+    {username: 'weegembump', name: 'Gemma Bump', avatar_url: 'https://myanimelist.cdn-dena.com/images/characters/3/66303.jpg'},
+    {username: 'jessjelly', name: 'Jess Jelly', avatar_url: 'https://dzt1km7tv28ex.cloudfront.net/u/396489973932490752_35s_d.jpg'}
+  ];
+  
