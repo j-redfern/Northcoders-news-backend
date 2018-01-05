1c9dc7aec99ef877bbbefd57eba4ff34341c066a
Jenny Redfern <jchunyi.ho@googlemail.com>
2018-01-05 13:38:51 +0000

    define route for getArticlesByTopicId

--- routes/topics.js
+++ routes/topics.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const {getAllTopics} = require('../controller/topics')
+const {getAllTopics, getArticlesByTopicId} = require('../controller/topics');
 
-router.use('/', getAllTopics);
+router.get ('/', getAllTopics);
+router.get('/:topic_id/articles', getArticlesByTopicId);
 
 module.exports = router;
