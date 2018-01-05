2ae923e2e1b9194c0198c17a87103b1c6b88e63e
Jenny Redfern <jchunyi.ho@googlemail.com>
2018-01-05 13:39:40 +0000

    define controller for getArticlesByTopicId

--- controller/topics.js
+++ controller/topics.js
@@ -1,13 +1,27 @@
-const {Topics} = require ('../models/models');
+const { Topics, Articles } = require('../models/models');
 
-const getAllTopics = (req,res, next) => {
+const getAllTopics = (req, res, next) => {
     Topics.find()
-    .then ((topics)=> {
-        res.send({topics})
-    })
-    .catch (err => {
-        next(err)
-    })
+        .then((topics) => {
+            res.send({ topics })
+        })
+        .catch(err => {
+            next(err);
+        })
 }
+const getArticlesByTopicId = (req, res, next) => {
+    Topics.findById(req.params.topic_id)
+        .then((topic) => {
+            return Articles.find()
+                .where("belongs_to")
+                .equals(topic.slug)
+        })
+        .then ((articles)=> {
+            res.send({articles})
+        })
+        .catch(err => {
+            next(err);
+        })
+} 
 
-module.exports = {getAllTopics}
+module.exports = { getAllTopics, getArticlesByTopicId };
