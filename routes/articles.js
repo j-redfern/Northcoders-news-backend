d1c52e7787d183e2f321d746a15ca80e1dbaa2bc
Jenny Redfern <jchunyi.ho@googlemail.com>
2018-01-08 15:42:34 +0000

    define route for editVotes for Articles

--- routes/articles.js
+++ routes/articles.js
@@ -1,10 +1,11 @@
 
 const router = require('express').Router();
 
-const {getAllArticles, getCommentsByArtId, addComments} = require('../controller/articles');
+const {getAllArticles, getCommentsByArtId, addComments, editVotes} = require('../controller/articles');
 
 router.get('/', getAllArticles);
 router.get('/:article_id/comments', getCommentsByArtId);
 router.post('/:article_id/comments', addComments);
+router.put('/:article_id', editVotes);
 
 module.exports = router;
