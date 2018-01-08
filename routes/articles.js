03b11afdd70519970a51537ca1ffb0313d019fbf
Jenny Redfern <jchunyi.ho@googlemail.com>
2018-01-08 15:37:34 +0000

    define route for addComments

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,10 @@
 
 const router = require('express').Router();
 
-const {getAllArticles, getCommentsByArtId} = require('../controller/articles');
+const {getAllArticles, getCommentsByArtId, addComments} = require('../controller/articles');
 
 router.get('/', getAllArticles);
 router.get('/:article_id/comments', getCommentsByArtId);
+router.post('/:article_id/comments', addComments);
 
 module.exports = router;
