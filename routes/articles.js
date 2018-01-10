a8642a013b826fec26858b020cf3159bb3cd2060
Jenny Redfern <jchunyi.ho@googlemail.com>
2018-01-10 18:09:22 +0000

    define route for getArticleByArtId

--- routes/articles.js
+++ routes/articles.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 const {getAllArticles, getCommentsByArtId, addComments, editVotes, getArticleByArtId} = require('../controller/articles');
 
 router.get('/', getAllArticles);
-route.get('/:article_id', getArticleByArtId);
+router.get('/:article_id', getArticleByArtId);
 router.get('/:article_id/comments', getCommentsByArtId);
 router.post('/:article_id/comments', addComments);
 router.put('/:article_id', editVotes);
