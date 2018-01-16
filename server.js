9f192b6a99d55dce9e315480a08dfe82ed4f57cc
Jenny Redfern <jchunyi.ho@googlemail.com>
2018-01-16 15:49:58 +0000

    implement error handling for 404, 400 and 500

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 var config = require('./config');
 var db = config.DB[process.env.NODE_ENV] || process.env.DB;
 mongoose.Promise = Promise;
-const apiRouter = require('./routes/api')
+const apiRouter = require('./routes/api');
 const cors = require('cors');
 
 mongoose.connect(db, {useMongoClient: true})
@@ -17,6 +17,24 @@ mongoose.connect(db, {useMongoClient: true})
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api',apiRouter)
+app.use('/api',apiRouter);
+
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    return res.status(404).send({error: "File Not Found"});
+  } next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err.status === 400) {
+    return res.status(400).send({error: "400 Bad Request"});
+  } next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err.status === 500) {
+    return res.status(500).send({error: "Internal Server Error"});
+  } next(err);  
+});
 
 module.exports = app;
