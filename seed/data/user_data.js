eb51d2853b4be84ce0489fd57b34e33abde091de
Jenny Redfern <jchunyi.ho@googlemail.com>
2018-01-16 17:00:56 +0000

    update url for user - paul grump

--- seed/data/user_data.js
+++ seed/data/user_data.js
@@ -1,6 +1,6 @@
 module.exports = [
     {username: 'tickle122', name: 'Tom Tickle', avatar_url: 'https://dreager1.files.wordpress.com/2013/05/luffy_anime_post_timeskip_infobox.png'},
-    {username: 'grumpy19', name: 'Paul Grump', avatar_url: 'https://vignette.wikia.nocookie.net/vsbattles/images/d/d0/Roronoa_Zoro_Pre_Timeskip.png/revision/latest?cb=20160327033108'},
+    {username: 'grumpy19', name: 'Paul Grump', avatar_url: 'https://myanimelist.cdn-dena.com/s/common/uploaded_files/1475495851-8a16656834c6f9bcd87221808811ee78.jpeg'},
     {username: 'happyamy2016', name: 'Amy Happy', avatar_url: 'https://orig00.deviantart.net/bfcd/f/2016/088/1/c/nami_one_piece__by_roxy_h___by_exsplo_d4ejmfq_by_yoos2210-d9wys7a.png'},
     {username: 'cooljmessy', name: 'Peter Messy', avatar_url: 'http://www.behindthevoiceactors.com/_img/chars/tony-tony-chopper-toriko-x-one-piece-collaboration-special-32.5.jpg'},
     {username: 'weegembump', name: 'Gemma Bump', avatar_url: 'https://myanimelist.cdn-dena.com/images/characters/3/66303.jpg'},
