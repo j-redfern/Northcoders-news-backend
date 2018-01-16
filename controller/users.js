6c24bc45b70942dde5a73abb3d5f46127a5c57bd
Jenny Redfern <jchunyi.ho@googlemail.com>
2018-01-16 15:52:13 +0000

    implement get all users and get user data by username function in controller

--- controller/users.js
+++ controller/users.js
@@ -1,11 +1,19 @@
-const { Users } = require('../models/models');
+const { Users } = require('../models');
+
+const getAllUsers = (req,res,next) => {
+    Users.find()
+    .then((users) => {
+        res.send(users);
+    })
+    .catch(err => next(err));
+};
 
 const getUserData = (req,res,next) => {
     Users.find({username : req.params.username})
     .then((users) => {
         res.send(users);
     })
-    .catch( err => next(err))
-}
+    .catch(err => next(err));
+};
 
-module.exports = {getUserData};
+module.exports = {getUserData, getAllUsers};
